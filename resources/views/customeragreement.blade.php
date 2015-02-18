ec46d48c0f730b22cad7fa3f32e50856eecbb7de
Joshua Gagnon <josh.n.gagnon@gmail.com>
2015-02-18 17:55:05 +1300

    marked up legal bs.  jeez

--- resources/views/customeragreement.blade.php
+++ resources/views/customeragreement.blade.php
@@ -1,7 +1,97 @@
 @extends('app')
-
 @section('content')
-
-blah
+<div class="container">
+<h2>CUSTOMER AGREEMENT</h2>
+<em>This Agreement sets out the terms governing the relationship between You and CataLex.  Please read it carefully. </em>
+<ol><li><h5>DEFINITIONS </h5><ol>
+<li><strong>"Agreement" </strong> means this Customer Agreement. </li>
+<li><strong>"Access Fee"</strong> means the fee (excluding any taxes and duties) payable by You for access to the Services, as set out on the Website (which CataLex may change from time to time on notice to You). </li>
+<li><strong>"CataLex"</strong> means CataLex Limited, a New Zealand company with company number 5311842.</li>
+<li><strong>"Confidential Information"</strong> includes all information exchanged between the parties to this Agreement, whether in writing, electronically or orally, but does not include information which is, or becomes, publicly available other than through unauthorised disclosure by the other party. </li>
+<li><strong>"Customer"</strong> means the person who registers to have access to the Services and, where the context permits, includes any entity or organisation on whose behalf that person registers to have access to the Services.</li>
+<li><strong>"Data"</strong> means any data inputted by You or with Your authority into the Website. </li>
+<li><strong>"Intellectual Property Right" </strong> means any patent, trade mark, service mark, copyright, moral right, right in a design, know-how and any other intellectual or industrial property rights, anywhere in the world whether or not registered. </li>
+<li><strong>"Privacy Policy"</strong> means CataLex’s privacy policy available on the Website and updated from time to time.</li>
+<li><strong>"Services"</strong> means the online legal services made available (as may be changed or updated from time to time by CataLex) via the Website, and <strong>Service</strong> means any one of them. </li>
+<li><strong>"Terms of Use"</strong> means the terms applicable to the use of any particular Service by a User.</li>
+<li><strong>"Trial Period"</strong> means the period of 14 days from (but not including) the date the Customer created 	their account with CataLex.</li>
+<li><strong>"Users"</strong> means the people with individual access to the Services as determined by the Customer, and <strong>"User"</strong> means any one of them.</li>
+<li><strong>"Website" </strong> means the Internet site at the domain www.catalex.nz or any other site operated by CataLex. </li>
+<li><strong>"You"</strong> means the Customer and, and where the context permits, Users. <strong>"Your"</strong>  has a corresponding meaning. </li></ol></li>
+<li><h5>USE OF THE SERVICES</h5><ol>
+<li><strong>Access and use: </strong>  CataLex grants You the right to access and use the Services.  This right is non-exclusive, non-transferable, and limited by and subject to this Agreement. </li>
+<li><strong>Trial Period: </strong>  You may access and use the Services free of charge during the Trial Period.  At the end of the Trial Period, You will have the option to either continue Your subscription or cancel under clause 9(a) below. </li>
+<li><strong>Users: </strong>  You acknowledge and agree that, subject to law and any applicable agreement between the Customer and Users:<ol>
+<li>the Customer determines who is a User and what kind of access to the Services that User has;</li>
+<li>the Customer is responsible for all Users’ use of the Services;</li>
+<li>the Customer controls each User’s access to the Services at all times and can revoke or change a User’s access at any time and for any reason, in which case that person or entity will cease to be a User or shall have that different level of access, as the case may be; and</li>
+<li>if there is any dispute between a Customer and a User regarding access to any Data or Service, the Customer shall decide what access to the relevant Data or Service that User shall have, if any. </li></ol></li></ol></li>
+<li><h5>YOUR OBLIGATIONS</h5><ol>
+<li><strong>Payment of Access Fee:</strong>  You must pay the Access Fee in order to have access and use of the Services after the Trial Period.  If You notify CataLex that You wish to continue Your subscription after the Trial Period, You authorise CataLex to charge the Access Fee, in advance, to the credit or debit card linked to the Your account with CataLex.  The Access Fee will be charged either monthly or annually at Your election.  On payment of the Access Fee, CataLex will issue a tax invoice and receipt confirming payment.  You are responsible for payment of all taxes and duties in addition to the Access Fee. </li>
+<li><strong>General obligations: </strong> You must only use the Services and Website for Your own lawful internal business purposes, in accordance with this Agreement and any Terms of Use. </li>
+<li><strong>Usernames and passwords: </strong>  You must ensure that all usernames and passwords required to access the Services are kept secure and confidential. You must immediately notify CataLex of any unauthorised use of Your passwords or any other breach of security.  On receiving that notification, CataLex will reset Your password and You must take all other actions that CataLex reasonably deems necessary to maintain or enhance the security of CataLex's computing systems, networks, and Your access to the Services. </li>
+<li><strong>Conditions of access and use: </strong>  As a condition of this Agreement, when accessing and using the 	Services, You must:<ol>
+<li>not attempt to undermine the security or integrity of CataLex's computing systems or networks or, where the Services are hosted by a third party, that third party's computing systems and networks;</li>
+<li>not use, or misuse, the Services in any way which may impair the functionality of the Services or Website, or other systems used to deliver the Services or impair the ability of any other user to use the Services or Website;</li>
+<li>not attempt to gain unauthorised access to any materials other than those to which You have been given express permission to access or to the computer system on which the Services are hosted;</li>
+<li>not transmit, or input into the Website, any files that may damage any other person's computing devices or software, content that may be offensive, or material or Data in violation of any law (including Data or other material protected by copyright or trade secrets which You do not have the right to use); and</li>
+<li>not attempt to modify, copy, adapt, reproduce, disassemble, decompile or reverse engineer any computer programs used to deliver the Services or to operate the Website except as is strictly necessary to use either of them for normal operation. </li></ol></li>
+<li><strong>Indemnity. </strong> You indemnify CataLex against all claims, costs, damage and loss arising from Your breach of any of this Agreement or any obligation You may have to CataLex, including (but not limited to) any costs relating to the recovery of any Access Fees that are due but have not been paid by You. </li></ol></li>
+<li><h5>CONFIDENTIALITY AND PRIVACY</h5><ol>
+<li><strong>Confidentiality: </strong>  Unless the relevant party has the prior written consent of the other or unless required to do so by law: 
+Each party will preserve the confidentiality of all Confidential Information of the other obtained in connection with this Agreement. Neither party will, without the prior written consent of the other, disclose or make any Confidential Information available to any person, or use the same for its own benefit, other than as contemplated by this Agreement.
+Each party's obligations under this clause will survive termination of this Agreement.
+The provisions of clauses 4(a)(i) and 4(a)(ii) shall not apply to any information which: (1) is or becomes public knowledge other than by a breach of this clause; (2) is received from a third party who lawfully acquired it and who is under no obligation restricting its disclosure; (3) is in the possession of the receiving party without restriction in relation to disclosure before the date of receipt from the disclosing party; or (4) is independently developed without access to the Confidential Information. </li>
+<li><strong>Privacy Policy: </strong>  You acknowledge and agree to the terms of our Privacy Policy. </li></ol></li>
+<li><h5>INTELLECTUAL PROPERTY</h5><ol>
+<li><strong>General: </strong> Title to, and all Intellectual Property Rights in the Services, the Website and any documentation relating to the Services remain the property of CataLex (or its licensors). </li>
+<li><strong>Ownership of Data: </strong> Title to, and all Intellectual Property Rights in, the Data remain Your property.  However, Your access to the Data is contingent on full payment of the Access Fee when due. You grant CataLex a licence to use, copy, transmit, store, and back-up Your information and Data for the purposes of enabling You to access and use the Services and for any other purpose related to provision of services to You. </li>
+<li><strong>Backup of Data: </strong> You must maintain copies of all Data inputted into the Service. CataLex adheres to its best practice policies and procedures to prevent data loss, including a daily system data back-up regime, but does not make any guarantees that there will be no loss of Data. CataLex expressly excludes liability for any loss of Data no matter how caused. </li></ol></li>
+<li><h5>WARRANTIES</h5><ol>
+<li><strong>Authority for other people: </strong> You warrant that where You have registered to use the Services on behalf of another person, You have the authority to agree to this Agreement on behalf of that person and agree that by registering to use the Service You bind the person on whose behalf You act to the performance of any and all obligations that You become subject to by virtue of this Agreement, without limiting Your own personal obligations under this Agreement. </li>
+<li><strong>No warranties: </strong> CataLex gives no warranty about the Services. Without limiting the foregoing, CataLex does not warrant that the Services will meet Your requirements or that it will be suitable for any particular purpose. To avoid doubt, all implied conditions or warranties are excluded in so far as is permitted by law, including (without limitation) warranties of merchantability, fitness for purpose, title and non-infringement. </li>
+<li><strong>Consumer guarantees: </strong> You warrant and represent that You are acquiring the right to access and use the Services for the purposes of a business and that, to the maximum extent permitted by law, any statutory consumer guarantees or legislation intended to protect non-business consumers in any jurisdiction does not apply to the supply of the Services, the Website or this Agreement. </li></ol></li>
+<li><h5>ACKNOWLEDGEMENTS</h5><ol>
+<li><strong>Authority to access: </strong>  You are authorised to use the Services and the Website and to access the information and Data that You input into the Website, including any information or Data input into the Website by any person you have authorised to use the Service. You are also authorised to access the processed information and Data that is made available to You through Your use of the Website and the Services (whether that information and Data is Your own or that of anyone else). </li>
+<li><strong>No responsibility to third parties: </strong>  CataLex has no responsibility to any person other than You and nothing in this Agreement confers, or purports to confer, a benefit on any person other than You. If You use the Services or access the Website on behalf of or for the benefit of anyone other than yourself (whether a body corporate or otherwise) you agree that:
+<ol><li>You are responsible for ensuring that You have the right to do so;</li>
+<li>You are responsible for authorising any person who is given access to information or Data, and you agree that CataLex has no obligation to provide any person access to such information or Data without Your authorisation and may refer any requests for information to You to address; and</li>
+<li>You will indemnify CataLex against any claims or loss relating to: (1) CataLex's refusal to provide any person access to Your information or Data in accordance with this Agreement; or (2) CataLex’s making available information or Data to any person with Your authorisation. </li></ol></li>
+<li><strong>Own Risk: </strong> The provision of, access to, and use of, the Services is on an "as is " basis and at Your own risk. </li>
+<li><strong>Interruptions and errors: </strong>  CataLex does not warrant that the use of the Service will be uninterrupted or error free. Among other things, the operation and availability of the systems used for accessing the Service, including public telephone services, computer networks and the Internet, can be unpredictable and may from time to time interfere with or prevent access to the Services. CataLex is not in any way responsible for any such interference or prevention of Your access or use of the Services. </li>
+<li><strong>Not a Lawyer: </strong>  CataLex is not Your lawyer and use of the Services does not constitute the receipt of legal advice. If You have any legal questions, please contact a lawyer. </li>
+<li><strong>Suitability: </strong>  It is Your sole responsibility to determine that the Services meet the needs of Your business and are suitable for the purposes for which they are used. </li>
+<li><strong>Compliance with law: </strong>  You remain solely responsible for complying with all applicable laws. It is Your responsibility to check that storage of and access to your Data via the Software and the Website will comply with laws applicable to you (including any laws requiring you to retain records). </li></ol></li>
+<li><h5>LIMITATION OF LIABILITY</h5><ol>
+<li><strong>Exclusion: </strong>  To the maximum extent permitted by law, CataLex excludes all liability and responsibility to You (or any other person) in contract, tort (including negligence), or otherwise, for any loss (including loss of information, Data, profits and savings) or damage resulting, directly or indirectly, from any use of, or reliance on, the Service or Website. </li>
+<li><strong>Limitation: </strong>  If You suffer loss or damage as a result of CataLex's negligence or failure to comply with this Agreement, any claim by You against CataLex arising from CataLex's negligence or failure will be limited in respect of any one incident, or series of connected incidents, to the Access Fees paid by You in the previous 12 months. </li>
+<li><strong>Remedy: </strong>  If You are not satisfied with the Service, Your sole and exclusive remedy is to terminate this Agreement in accordance with clause 9. </li></ol></li>
+<li><h5>TERMINATION</h5><ol>
+<li><strong>Termination within Trial period: </strong>  You may terminate this Agreement after the Trial Period by giving notice to CataLex. </li>
+<li><strong>No-fault termination: </strong> This Agreement will continue for the period covered by the Access Fee paid under clause 3(a). At the end of each billing period this Agreement will automatically continue for another period of the same duration as that period, unless either party terminates this Agreement by giving notice to the other party at least 14 days before the end of the relevant period. </li>
+<li><strong>No refunds: </strong>  CataLex is not under any obligation to refund Access Fees that the Customer paid prior to termination of this Agreement. </li>
+<li><strong>Breach: </strong> If You:<ol>
+<li>breach any of this Agreement and do not remedy the breach within 14 days after receiving notice of the breach if the breach is capable of being remedied;</li>
+<li>breach any of this Agreement and the breach is not capable of being remedied; or</li>
+<li>You or Your business become insolvent or Your business goes into liquidation or has a receiver or manager appointed of any of its assets or if You become insolvent, or make any arrangement with Your creditors, or become subject to any similar insolvency event in any jurisdiction,</li></ol>
+	CataLex may take any or all of the following actions, at its sole discretion: 
+<ol><li>Terminate this Agreement and Your use of the Services and the Website;</li>
+<li>Suspend for any definite or indefinite period of time, Your use of the Services and the Website;</li>
+<li>Suspend or terminate access to all or any Data.</li>
+<li>Take either of the actions in sub-clauses (d), (e) and (f) of this clause 9(d) in respect of any or all other persons whom You have authorised to have access to Your information or Data.</li></ol></li>
+<li><strong>Accrued Rights</strong>: Termination of this Agreement is without prejudice to any rights and obligations of the parties accrued up to and including the date of termination. On termination of this Agreement You will:<ol>
+<li>remain liable for any accrued charges and amounts which become due for payment before or after termination; and</li>
+<li>immediately cease to use the Services and the Website.</li></ol></li>
+<li><strong>Expiry or termination:</strong> Clause 3(a) and clauses 4 to 10 survive the expiry or termination of this Agreement.</li></ol></li>
+<li><h5>GENERAL</h5><ol>
+<li><strong>Entire agreement:</strong> This Agreement, together with any Terms of Use, supersede and extinguish all prior agreements, representations (whether oral or written), and understandings and constitute the entire agreement between You and CataLex relating to the Services and the other matters dealt with in this Agreement.</li>
+<li><strong>Waiver:</strong> If either party waives any breach of this Agreement, this will not constitute a waiver of any other breach. No waiver will be effective unless made in writing.</li>
+<li><strong>Delays:</strong> Neither party will be liable for any delay or failure in performance of its obligations under this Agreement if the delay or failure is due to any cause outside its reasonable control. This clause does not apply to any obligation to pay money.</li>
+<li><strong>No Assignment:</strong> You may not assign or transfer any rights to any other person without CataLex's prior written consent.</li>
+<li><strong>Governing law and jurisdiction:</strong> Any dispute arising in connection with this Agreement of use will be governed by New Zealand law and resolved exclusively in the New Zealand courts.  </li>
+<li><strong>Severability:</strong> If any part or provision of this Agreement is invalid, unenforceable or in conflict with the law, that part or provision is replaced with a provision which, as far as possible, accomplishes the original purpose of that part or provision. The remainder of this Agreement will be binding on the parties.</li>
+<li><strong>Notices:</strong> Any notice given under this Agreement by either party to the other must be in writing by email and will be deemed to have been given on transmission.  Notices to CataLex must be sent to mail@catalex.nz or to any other email address notified by email to You by CataLex. Notices to You will be sent to the email address which You provided when setting up Your access to the Services.</li>
+<li><strong>Rights of Third Parties:</strong> A person who is not a party to this Agreement has no right to benefit under or to enforce any term of this Agreement.</li></ol></li></ol>
+</div>
 
 @endsection
--- resources/views/termsofuse.blade.php
+++ resources/views/termsofuse.blade.php
@@ -1,7 +1,21 @@
 @extends('app')
 
 @section('content')
-
-blah
-
+<div class="container">
+<h3>Law Browser</h3>
+<h2>TERMS OF USE</h2>
+<strong>Between</strong>: CataLex Limited (<strong>CataLex</strong>) and you, the user (<strong>You</strong>)
+<ol>
+<li><strong>CataLex is not a lawyer</strong>
+<p>You acknowledge and agree that CataLex is not, and has not held itself out as being (or being able to replace), a lawyer or an incorporated law firm, and that no lawyer-client or other fiduciary relationship of any kind exists or can arise between You and CataLex.</p></li>
+<li><strong>Law Browser is not an official source of law</strong>
+<p>You acknowledge that Law Browser is not an official source of New Zealand law.</p></li>
+<li><strong>All care, no responsibility</strong>
+<p>
+Although CataLex has taken care to ensure the content provided through Law Browser is accurate in all respects and in compliance with applicable standards, You agree that your use of and reliance on Law Browser is entirely Your responsibility.  You agree that You will have no claim against CataLex for any kind of loss or damage arising in connection with Your use of Law Browser.</p></li>
+<li><strong>Coverage and Source</strong><p>
+Law Browser is limited in coverage to New Zealand legislation reproduced from http://www.legislation.govt.nz, and case law reproduced from the Ministry of Justice’s ‘Judicial Decisions Online’ database at https://forms.justice.govt.nz/jdo/Search.jsp.  The terms and conditions for the use of those resources may apply or be relevant to Your use of Law Browser.</p></li>
+<li><strong>Crown Copyright</strong><p>
+All legislation and case law displayed in Law Browser is subject to Crown copyright.</p></li></ol>
+</div>
 @endsection
