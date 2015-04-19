974769e32ac68cf1028d30af1b0121c7c30268fd
Matthew Cook <mgcook89@gmail.com>
2015-04-20 10:11:05 +1200

    Style up application 'down' page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,41 +1,42 @@
-<html>
-	<head>
-		<link href='http://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
-
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
-
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
-
-			.title {
-				font-size: 72px;
-				margin-bottom: 40px;
-			}
-		</style>
-	</head>
-	<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>CataLex Law Browser - Temporarily Unavailable</title>
+	{{-- TODO: <link rel="shortcut icon" type="image/png" href="/favicon.png" /> --}}
+	<link href="/css/app.css" rel="stylesheet">
+	<link href='//brick.a.ssl.fastly.net/Ubuntu:400,400i,500,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<nav class="navbar navbar-default">
 		<div class="container">
-			<div class="content">
-				<div class="title">Be right back.</div>
+			<div class="navbar-header">
+				<a class="pull-left" href="#"><img alt="CataLex" src="/images/law-browser.png"></a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="app-navbar-collapse">
+				<img class="chev-left" src="/images/left-chevron.png"/><img class="chev-right" src="/images/right-chevron.png"/>
+				<ul class="nav navbar-nav navbar-right"></ul>
 			</div>
 		</div>
-	</body>
+	</nav>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h2>CataLex is currently unavailable due to maintenance</h2>
+				<p>Sorry for the inconvenience. Please try again in just a few moments...</p>
+			</div>
+		</div>
+	</div>
+	<footer class="footer">
+		<div class="container text-center">
+			<p class="copyright">© Copyright {{ date('Y') }} - CataLex Limited. All rights reserved.</p>
+			<p class="links"><a href="/customeragreement">Customer Agreement</a><a href="/privacypolicy">Privacy Policy</a><a href="/termsofuse">Law Browser Terms of Use</a></p>
+			<p>P: C/- Kanu Jeram Chartered Accountant Limited, 112 Kitchener Road, Milford, Auckland, 0620, New Zealand</p>
+			<p>E: <a href="mailto:mail@catalex.nz">mail@catalex.nz</a> &nbsp; M: +64 274 538 552 &nbsp; Fax: +64 9 929 3332</p>
+		</div>
+	</footer>
+</body>
 </html>
