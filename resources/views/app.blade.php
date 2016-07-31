ab8e7f65371db629f1cd51282c29b89ad47685dc
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-08-01 10:38:30 +1200

    added images button for login

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,7 +26,7 @@
 	<nav class="navbar navbar-default">
 		<div class="container">
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse"  aria-expanded="false">
 					<span class="sr-only">Toggle Navigation</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,24 +5,30 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
+				<div class="panel-heading">CataLex Home</div>
 				<div class="panel-body">
-					<p>Welcome to CataLex</p>
-					@if($user->hasBrowserAccess())
-						<p><a href="{{ route('browser-login') }}">Go to Law Browser</a></p>
-					@elseif($user->everBilled()/*TODO: && $user->can('editpaymentdetails')*/)
-						<p>You last billing cycle has not completed correctly. Please <a href="#TODO">click here</a> to update your payment details.</p>
-					@else
-						<p>Your trial period has expired. Please <a href="{{ action('BillingController@getStart') }}">click here</a> to confirm your subscription to regain access to Law Browser.</p>
-					@endif
+					<p>Welcome to CataLex.</p>
+                    <div class="row services">
+                        <div class="col-md-6 service">
+    					@if($user->hasBrowserAccess())
+    						<a href="{{ route('browser-login') }}"><span class="image-button"><img src="/images/law-browser.png" /></span><br/>Go to Law Browser</a></p>
+    					@elseif($user->everBilled()/*TODO: && $user->can('editpaymentdetails')*/)
+    						<p>You last billing cycle has not completed correctly. Please <a href="#TODO">click here</a> to update your payment details.</p>
+    					@else
+    						<p>Your trial period has expired. Please <a href="{{ action('BillingController@getStart') }}">click here</a> to confirm your subscription to regain access to Law Browser.</p>
+    					@endif
+                        </div>
+                        <div class="col-md-6 service">
 
-                    @if($user->hasGoodCompaniesAccess())
-                        <p><a href="{{ route('good-companies-login') }}">Go to Good Companies</a></p>
-                    @elseif($user->everBilled()/*TODO: && $user->can('editpaymentdetails')*/)
-                        <p>You last billing cycle has not completed correctly. Please <a href="#TODO">click here</a> to update your payment details.</p>
-                    @else
-                        <p>Your trial period has expired. Please <a href="{{ action('BillingController@getStart') }}">click here</a> to confirm your subscription to regain access to Good Companies.</p>
-                    @endif
+                        @if($user->hasGoodCompaniesAccess())
+                            <p><a href="{{ route('good-companies-login') }}"><span class="image-button"><img src="/images/good-company-lg.png"/></span><br/>Go to Good Companies</a></p>
+                        @elseif($user->everBilled()/*TODO: && $user->can('editpaymentdetails')*/)
+                            <p>You last billing cycle has not completed correctly. Please <a href="#TODO">click here</a> to update your payment details.</p>
+                        @else
+                            <p>Your trial period has expired. Please <a href="{{ action('BillingController@getStart') }}">click here</a> to confirm your subscription to regain access to Good Companies.</p>
+                        @endif
+                        </div>
+                    </div>
 				</div>
 			</div>
 		</div>
