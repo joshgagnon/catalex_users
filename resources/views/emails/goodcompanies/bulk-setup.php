90f5df93e7b981125cc90b6d55b5216fa1478735
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-10-12 08:56:04 +1300

    added new email

--- resources/views/emails/goodcompanies/bulk-setup.php
+++ resources/views/emails/goodcompanies/bulk-setup.php
@@ -12,7 +12,7 @@
                                 <td>
                                     <h2 class="center">Companies Imported</h2>
                                     <p>Hi {{ $name }}</p>
-                                    <p>Good Companies has successfully bulk imported <strong>{{ $successCount }}</strong> of <strong>{{ $totalCount }}</strong> companies.  You can view them <a href={{ $link }}>here</a>.</p>
+                                    <p>Good Companies has successfully applied share classes and imported history for <strong>{{ $successCount }}</strong> of <strong>{{ $totalCount }}</strong> companies.  You can view them <a href={{ $link }}>here</a>.</p>
                                     <p>Best regards</p>
                                     <p>The CataLex team</p>
                                     <p><a href="mailto:mail@catalex.nz">mail@catalex.nz</a></p>
