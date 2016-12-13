da950780917e83916656351182919e9892ddd089
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-12-14 10:11:14 +1300

    updated text on subscription email

--- resources/views/emails/subscription.blade.php
+++ resources/views/emails/subscription.blade.php
@@ -13,6 +13,8 @@
                                     <p>Hi {{ $name }},</p>
                                     <p>Thanks for subscribing to Good Companies</p>
                                     <p>You can now add companies to your account by importing their details from the New Zealand companies register.  Click <a href="{{ route('good-companies-login') }}">here</a> to start.</p>
+                                    <p>In 14 days, we will charge your credit/debit card based on the number of companies connected to your account on that day, and your chosen billing period.  Additional charges will be made if companies are added to your account within the billing period.</p>
+
                                     <p>The CataLex team</p>
                                     <p><a href="mailto:mail@catalex.nz">mail@catalex.nz</a></p>
                                 </td>
