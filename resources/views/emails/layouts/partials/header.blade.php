1dad37047837796ad71bd63982b76773b21ee001
Paddy Moran <pdmoran@outlook.com>
2017-01-23 15:00:51 +1300

    Changed layout of attached documents email to target it toward non-members

---
+++ resources/views/emails/layouts/partials/header.blade.php
@@ -0,0 +1,19 @@
+<table class="container header">
+    <tr>
+        <td>
+            <table class="row">
+                <tr>
+                    <td class="wrapper last">
+                        <table class="twelve columns">
+                            <tr>
+                                <td>
+                                    {!! Html::image('/images/email/header.png', 'CataLex') !!}
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
---
+++ resources/views/emails/layouts/partials/non-member-footer.blade.php
@@ -0,0 +1,21 @@
+<table class="container footer">
+    <tr>
+        <td>
+            <table class="row">
+                <tr>
+                    <td class="wrapper last">
+                        <table class="twelve columns">
+                            <tr>
+                                <td>
+                                    <center>
+                                        <p class="center">Get your company administration in order with Good Companies <a href="https://users.catalex.nz/my-services?Good%2BCompanies=1">click here.</a></p>
+                                    </center>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
