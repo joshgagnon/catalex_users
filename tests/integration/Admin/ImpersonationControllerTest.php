7e4f8636752dea3ddf61ae36ffa335fd5efe85c1
Paddy Moran <pdmoran@outlook.com>
2017-03-16 10:08:14 +1300

    Testing for impersonation

---
+++ tests/integration/Admin/ImpersonationControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Role;
+use Auth;
+
+class ImpersonationControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    protected $seeder = 'DatabaseSeeder';
+
+    /**
+     * @test
+     */
+    public function impersonation()
+    {
+        // Create admin
+        $admin = $this->createUser(['name' => 'admin']);
+        $admin->addRole(Role::where('name', '=', 'global_admin')->first());
+
+        // Create non-admin
+        $nonAdmin = $this->createUser(['name' => 'non-admin', 'email' => 'non-admin@gmail.com']);
+
+        // Login as admin
+        Auth::login($admin);
+
+        // Start impersonation of non admin
+        $this->visit('/admin/users')
+            ->press('Login');
+
+        // Check we are now impersonating the non-admin user
+        $this->seePageIs('/')
+            ->see('You are currently impersonating')
+            ->see('Return to Admin')
+            ->assertEquals($nonAdmin->id, Auth::id());
+
+        // Check we can navigate while impersonating
+        $this->visit('/organisation')
+            ->assertEquals($nonAdmin->id, Auth::id());
+
+        // Check we can return to the admin user
+        $this->press('Return to Admin')
+            ->seePageIs('/admin/users')
+            ->see('Logged out of')
+            ->assertEquals($admin->id, Auth::id());;
+    }
+}
