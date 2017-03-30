949ea57573f4a2ed3a9087c5053bd7fdac3c0f21
Paddy Moran <pdmoran@outlook.com>
2017-03-31 10:14:20 +1300

    Added billing failed banner and stopped access to GC when billing has failed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,14 +29,13 @@ class HomeController extends Controller {
 		$this->middleware('auth');
 	}
 
-    /**
-     * Show the application dashboard to the user.
-     *
-     * @return Response
-     */
     public function index()
     {
-        return view('home');
+        $subscriptionUpToDate = Auth::user()->subscriptionUpToDate();
+
+        return view('home')->with([
+            'subscriptionUpToDate' => $subscriptionUpToDate,
+        ]);
     }
 
     public function getBrowserLogin() {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,85 +2,103 @@
 
 @section('content')
 
+<div class="container">
+    @include('components.messages')
+</div>
+
+@if (!$subscriptionUpToDate)
     <div class="container">
-        @include('components.messages')
+        <div class="alert alert-danger">
+            <h3 class="alert-heading">Your last bill failed.</h3>
+            <p>Please click below to update your billing details, before you can access your paid services.</p>
+
+            <a href="{{ route('billing.edit') }}" class="btn btn-danger">Update Billing</a>
+        </div>
+    </div>
+@endif
+
+<div class="services container">
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="service">
+                <a href="{{ route('browser-login') }}">
+                    <i class="fa fa-search"></i>
+
+                    <h4>Law Browser</h4>
+                    <p>Find law faster and easier than ever before. Definition recognition, automatic cross-references, side-by-side browsing, and more!</p>
+                 </a>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            @if ($user->hasGoodCompaniesAccess())
+
+                @if ($subscriptionUpToDate)
+                    <div class="service">
+                        <a href="{{ route('good-companies-login') }}">
+                            <i class="fa fa-briefcase"></i>
+                            <h4>Good Companies</h4>
+                            <p>Takes care of a company’s disclosure and administrative requirements under the Companies Act 1993.</p>
+                        </a>
+                    </div>
+                @else
+                    <div class="disabled-service">
+                        <a href="{{ route('good-companies-login') }}">
+                            <i class="fa fa-briefcase"></i>
+                            <h4>Good Companies</h4>
+                            <p>Takes care of a company’s disclosure and administrative requirements under the Companies Act 1993.</p>
+                        </a>
+
+                        <p class="dont-gray">Your last bill failed. Please click below to update your billing details, before you can access Good Companies.</p>
+                        <a class="dont-gray btn btn-danger" href="{{ route('billing.edit') }}">Update Billing</a>
+                    </div>
+                @endif
+            @else
+                <div class="disabled-service">
+                    <div>
+                        <i class="fa fa-briefcase"></i>
+
+                        <h4>Good Companies</h4>
+                        <p>Takes care of a company’s disclosure and administrative requirements under the Companies Act 1993.</p>
+                    </div>
+
+                    <p><a href="{{ route('user-services.index', array(urlencode('Good Companies') => 1)) }}">Click here to subscribe</a></p>
+                    <p><a href="https://catalex.nz/good-companies.html" target="_blank">Click here for features and pricing</a></p>
+                </div>
+            @endif
+        </div>
+
     </div>
 
 
+    <div class="row">
+        <div class="col-md-6">
+            <div class="service">
+            <a href="http://workingdays.catalex.nz/">
+                 <i class="fa fa-calendar"></i>
+                <h4>Working Days</h4>
+                <p>
+                    Calculates legal deadlines based on common definitions of “working day”. 
+                </p>
+                 </a>
+            </div>
+        </div>
 
-                    <div class="services container">
+        <div class="col-md-6">
+            <div class="service">
+            <a href="https://concat.catalex.nz/">
+                 <i class="fa fa-copy"></i>
+                <h4>ConCat</h4>
+                <p>
+                    Concatenates (combines) PDF documents quickly and securely.
+                </p>
+                </a>
+            </div>
+        </div>
 
-                     <div class="row">
-                        <div class="col-md-6">
-                            <div class="service">
-                            <a href="{{ route('browser-login') }}">
-                                 <i class="fa fa-search"></i>
-                                <h4>Law Browser</h4>
-                                <p>
-                                    Find law faster and easier than ever before.  Definition
-                                    recognition, automatic cross-references, side-by-side
-                                    browsing, and more!
-                                </p>
-                                 </a>
-                            </div>
-                        </div>
+    </div>
 
-
-
-
-                        <div class="col-md-6">
-                            @if ($user->hasGoodCompaniesAccess())
-                            <div class="service">
-                            <a href="{{ route('good-companies-login') }}">
-                                <i class="fa fa-briefcase"></i>
-                                <h4>Good Companies</h4>
-                                <p>Takes care of a company’s disclosure and administrative requirements under the Companies Act 1993.</p>
-                            </a>
-                            @else
-                            <div class="disabled-service">
-                                <div class="">
-                                    <i class="fa fa-briefcase"></i>
-                                    <h4>Good Companies</h4>
-                                    <p>Takes care of a company’s disclosure and administrative requirements under the Companies Act 1993.</p>
-                                </div>
-                            <p><a href="{{ route('user-services.index', array(urlencode('Good Companies') => 1)) }}">Click here to subscribe</a></p>
-                            <p><a href="https://catalex.nz/good-companies.html" target="_blank">
-                                Click here for features and pricing
-                            </a></p>
-                            @endif
-                            </div>
-                        </div>
-
-                    </div>
-
-
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="service">
-                            <a href="http://workingdays.catalex.nz/">
-                                 <i class="fa fa-calendar"></i>
-                                <h4>Working Days</h4>
-                                <p>
-                                    Calculates legal deadlines based on common definitions of “working day”. 
-                                </p>
-                                 </a>
-                            </div>
-                        </div>
-
-                        <div class="col-md-6">
-                            <div class="service">
-                            <a href="https://concat.catalex.nz/">
-                                 <i class="fa fa-copy"></i>
-                                <h4>ConCat</h4>
-                                <p>
-                                    Concatenates (combines) PDF documents quickly and securely.
-                                </p>
-                                </a>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    </div>
+</div>
 
 @endsection
