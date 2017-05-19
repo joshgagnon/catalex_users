7d4c3d83ce079b43d813d871b2ef603746fe39d5
Paddy Moran <pdmoran@outlook.com>
2017-05-19 13:42:47 +1200

    Added total_before_discount column to charge log

---
+++ database/migrations/2017_05_19_112130_add_total_amount_after_discount_to_charge_log.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\ChargeLog;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTotalAmountAfterDiscountToChargeLog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('charge_logs', function(Blueprint $table) {
+            $table->string('total_before_discount', 16)->nullable()->default(null);
+        });
+        
+        $existingChargeLogs = ChargeLog::get();
+        
+        foreach ($existingChargeLogs as $chargeLog) {
+            $chargeLog->update(['total_before_discount' => $chargeLog->total_amount]);
+        }
+    
+    
+        Schema::table('charge_logs', function(Blueprint $table) {
+            $table->string('total_before_discount', 16)->nullable(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('charge_logs', function(Blueprint $table) {
+            $table->dropColumn('total_before_discount');
+        });
+    }
+}
