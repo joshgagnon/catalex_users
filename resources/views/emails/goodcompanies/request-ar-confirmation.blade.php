61917f0d0fedf300f9155fa3417764b498a59441
Joshua Gagnon <josh.n.gagnon@gmail.com>
2017-07-10 10:23:01 +1200

    fixed typo

--- resources/views/emails/goodcompanies/request-ar-confirmation.blade.php
+++ resources/views/emails/goodcompanies/request-ar-confirmation.blade.php
@@ -10,7 +10,7 @@
                         <table class="twelve columns">
                             <tr>
                                 <td>
-                                    <h2 class="center">Request to Confirm Annual Rreturn</h2>
+                                    <h2 class="center">Request to Confirm Annual Return</h2>
                                     <p>Hi {{ $name }}</p>
                                     <p>{{ $inviter }} would like you to confirm the details of <strong>{{ $companyName }}</strong> so its annual return can be filed with the Companies Office.</p>
                                     <p>Please review the company’s details within Good Companies <a href={{ $link }}>here</a>.   If the details are correct, click confirm.  If not, please note the required corrections in the form and submit the changes to {{ $inviter }} for updating.</p>
