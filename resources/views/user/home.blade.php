50224e5991ade7d2e7d47efdd632540b35a22451
Paddy Moran <pdmoran@outlook.com>
2017-08-16 10:21:22 +1200

    re-indent home view

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -2,41 +2,41 @@
 
 @section('content')
 
-<div class="container">
-    @include('components.messages')
-</div>
-
-<div class="container">
-    @include('billing.billing-alert')
-</div>
-
-
-@if (!$user->organisation && $userHasPendingInvite)
     <div class="container">
-        <div class="alert alert-info inline clearfix">
-            <a href="{{ route('organisation-invites.index') }}" class="btn btn-info">View invite</a>
-            <p>You have a pending organisation invite.</p>
-        </div>
+        @include('components.messages')
     </div>
-@endif
 
-<div class="services container">
-<div class="container">
-    <h2 class="page-title">CataLex Home</h2>
-</div>
-    <div class="row">
-        <div class="col-md-4">
-            <div class="user-menu-item panel panel-default ">
-                <div class="top-menu">
-                <a href="{{ route('index') }}">
-                    <i class="fa fa-briefcase"></i>
+    <div class="container">
+        @include('billing.billing-alert')
+    </div>
 
-                    <h4>CataLex Services</h4>
-                 </a>
-                 </div>
-                 <div class="bottom-menu">
-                     <div class="section summary"><span class="sub-title">Access CataLex services</span></div>
-                     <a href="{{ route('browser-login') }}" class="section ">
+
+    @if (!$user->organisation && $userHasPendingInvite)
+        <div class="container">
+            <div class="alert alert-info inline clearfix">
+                <a href="{{ route('organisation-invites.index') }}" class="btn btn-info">View invite</a>
+                <p>You have a pending organisation invite.</p>
+            </div>
+        </div>
+    @endif
+
+    <div class="services container">
+        <div class="container">
+            <h2 class="page-title">CataLex Home</h2>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="user-menu-item panel panel-default ">
+                    <div class="top-menu">
+                        <a href="{{ route('index') }}">
+                            <i class="fa fa-briefcase"></i>
+
+                            <h4>CataLex Services</h4>
+                        </a>
+                    </div>
+                    <div class="bottom-menu">
+                        <div class="section summary"><span class="sub-title">Access CataLex services</span></div>
+                        <a href="{{ route('browser-login') }}" class="section ">
                         <span class="with-icon">
                             <i class="fa fa-search"></i>
                             <span class="title">
@@ -44,8 +44,8 @@
                                 <span class="sub-title">Find New Zealand law faster</span>
                             </span>
                         </span>
-                     </a>
-                     <a href="{{ route('good-companies-login') }}" class="section">
+                        </a>
+                        <a href="{{ route('good-companies-login') }}" class="section">
                         <span class="with-icon">
                             <i class="fa fa-briefcase"></i>
                             <span class="title">
@@ -53,26 +53,26 @@
                                 <span class="sub-title">Maintain legally compliant companies</span>
                                 </span>
                         </span>
-                    </a>
-                    <a href="http://workingdays.catalex.nz/" class="section"><span class="with-icon"><i class="fa fa-calendar"></i><span class="title"><span class="main-title">Working Days</span><span class="sub-title">Calculate legal deadlines</span></span></span></a>
-                    <a href="https://concat.catalex.nz/" class="section"><span class="with-icon"><i class="fa fa-copy"></i><span class="title"><span class="main-title">ConCat</span><span class="sub-title">Combine PDF documents</span></span></span></a>
+                        </a>
+                        <a href="http://workingdays.catalex.nz/" class="section"><span class="with-icon"><i class="fa fa-calendar"></i><span class="title"><span class="main-title">Working Days</span><span class="sub-title">Calculate legal deadlines</span></span></span></a>
+                        <a href="https://concat.catalex.nz/" class="section"><span class="with-icon"><i class="fa fa-copy"></i><span class="title"><span class="main-title">ConCat</span><span class="sub-title">Combine PDF documents</span></span></span></a>
 
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-md-4">
-            <div class="user-menu-item panel panel-default ">
-                <div class="top-menu">
-                <a href="{{ route('user.profile') }}">
-                    <i class="fa fa-id-card"></i>
+            <div class="col-md-4">
+                <div class="user-menu-item panel panel-default ">
+                    <div class="top-menu">
+                        <a href="{{ route('user.profile') }}">
+                            <i class="fa fa-id-card"></i>
 
-                    <h4>My Account</h4>
-                 </a>
-                 </div>
-                 <div class="bottom-menu">
-                     <div class="section summary"><span class="sub-title">Manage your account and subscriptions</span></div>
-                     <a href="{{ route('user.profile') }}" class="section ">
+                            <h4>My Account</h4>
+                        </a>
+                    </div>
+                    <div class="bottom-menu">
+                        <div class="section summary"><span class="sub-title">Manage your account and subscriptions</span></div>
+                        <a href="{{ route('user.profile') }}" class="section ">
                         <span class="with-icon">
                             <i class="fa fa-user"></i>
                             <span class="title">
@@ -80,8 +80,8 @@
                                 <span class="sub-title">Set email address and password</span>
                             </span>
                         </span>
-                     </a>
-                     <a href="{{ route('user-services.index') }}" class="section">
+                        </a>
+                        <a href="{{ route('user-services.index') }}" class="section">
                         <span class="with-icon">
                             <i class="fa fa-check-square"></i>
                             <span class="title">
@@ -89,8 +89,8 @@
                                 <span class="sub-title">Manage CataLex service subscriptions</span>
                                 </span>
                         </span>
-                    </a>
-                     <a href="{{ route('billing') }}" class="section">
+                        </a>
+                        <a href="{{ route('billing') }}" class="section">
                         <span class="with-icon">
                             <i class="fa fa-credit-card"></i>
                             <span class="title">
@@ -98,24 +98,24 @@
                                 <span class="sub-title">View invoices and manage billing details</span>
                                 </span>
                         </span>
-                    </a>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
 
 
-        <div class="col-md-4">
-            <div class="user-menu-item panel panel-default ">
-                <div class="top-menu">
-                <a href="{{ route('organisation.index') }}">
-                    <i class="fa fa-users"></i>
+            <div class="col-md-4">
+                <div class="user-menu-item panel panel-default ">
+                    <div class="top-menu">
+                        <a href="{{ route('organisation.index') }}">
+                            <i class="fa fa-users"></i>
 
-                    <h4>Organisation</h4>
-                 </a>
-                 </div>
-                 <div class="bottom-menu">
-                     <div class="section summary"><span class="sub-title">Create and manage your organisation</span></div>
-                     <a href="{{ route('organisation.index') }}" class="section ">
+                            <h4>Organisation</h4>
+                        </a>
+                    </div>
+                    <div class="bottom-menu">
+                        <div class="section summary"><span class="sub-title">Create and manage your organisation</span></div>
+                        <a href="{{ route('organisation.index') }}" class="section ">
                         <span class="with-icon">
                             <i class="fa fa-user-plus"></i>
                             <span class="title">
@@ -123,16 +123,16 @@
                                 <span class="sub-title">Invite and view organisation members </span>
                             </span>
                         </span>
-                     </a>
+                        </a>
 
+                    </div>
                 </div>
             </div>
+
+
+
         </div>
 
-
-
     </div>
-
-</div>
 
 @endsection
