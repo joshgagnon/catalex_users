d6da7d2b4ec75f4e8a0f6f1b9b901467b830505f
Paddy Moran <pdmoran@outlook.com>
2017-08-16 11:26:21 +1200

    Add option to subscribe to CataLex Sign

--- resources/views/service-user/edit.blade.php
+++ resources/views/service-user/edit.blade.php
@@ -12,6 +12,7 @@ CataLex - Edit Services
             <div class="panel panel-default">
                 <div class="panel-body">
                     @include('components.messages')
+
                     <form method="POST" role="form" class="form">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <input type="hidden" name="user_id" value="{{ $user->id }}">
@@ -23,70 +24,74 @@ CataLex - Edit Services
 
                         <div class="form-group">
                             @foreach($services as $service)
-                                <div class="service-well well ">
-                                <div class="row">
-                                    <div class="col-xs-2">
-                                    <label>
-                                        <input
-                                            type="checkbox"
-                                            value="{{ $service->id }}"
-                                            name="services[{{ $service->id }}]"
-                                            {{ $service->userHasService || !$service->is_paid_service || isset($_GET[urlencode($service->name)]) ? 'checked' : '' }}
-                                            {{ !$service->is_paid_service ? 'disabled' : '' }}>
+                                <div class="service-well well">
+                                    <div class="row">
+                                        <div class="col-xs-2">
+                                            <label>
+                                                <input
+                                                    type="checkbox"
+                                                    value="{{ $service->id }}"
+                                                    name="services[{{ $service->id }}]"
+                                                    {{ $service->userHasService || !$service->is_paid_service || isset($_GET[urlencode($service->name)]) ? 'checked' : '' }}
+                                                    {{ !$service->is_paid_service ? 'disabled' : '' }}>
 
-                                    </label>
-                                    </div>
-                                    @if($service->name == 'Good Companies')
-                                        <div class="col-xs-10">
-                                            <h2>
-                                            <i class="fa fa-briefcase"></i>
-                                             {{ $service->name }}
-                                            </h2>
-                                        <div>
-                                        <p>Takes care of a company’s disclosure and administrative requirements under the Companies Act 1993.</p>
-                                        <p>Good Companies costs just <strong>$12 annually</strong> or <strong>$1.50 monthly</strong>, per company.  Click <a href="https://catalex.nz/good-companies.html" target="_blank">here</a> to see features.</p>
-                                    @endif
+                                            </label>
+                                        </div>
 
-                                    @if($service->name == 'Law Browser')
-                                        <div class="col-xs-10">
-                                            <h2>
-                                            <i class="fa fa-search"></i>
+                                        @if($service->name == 'Good Companies')
+                                            <div class="col-xs-10">
+                                                <h2><i class="fa fa-briefcase"></i> Good Companies</h2>
 
+                                                <div>
+                                                    <p>Takes care of a company’s disclosure and administrative requirements under the Companies Act 1993.</p>
+                                                    <p>Good Companies costs just <strong>$12 annually</strong> or <strong>$1.50 monthly</strong>, per company.  Click <a href="https://catalex.nz/good-companies.html" target="_blank">here</a> to see features.</p>
+                                                </div>
+                                            </div>
+                                        @endif
 
-                                            {{ $service->name }}
-                                            </h2>
-                                        <div>
+                                        @if($service->name == 'Law Browser')
+                                            <div class="col-xs-10">
+                                                <h2><i class="fa fa-search"></i> Law Browser</h2>
 
-                                        <p>Takes care of a company’s disclosure and administrative requirements under the Companies Act 1993.</p>
-                                        <p>This service is <strong>FREE</strong>, with added enhancements for CataLex users.</p>
-                                    @endif
+                                                <div>
+                                                    <p>Takes care of a company’s disclosure and administrative requirements under the Companies Act 1993.</p>
+                                                    <p>This service is <strong>FREE</strong>, with added enhancements for CataLex users.</p>
+                                                </div>
+                                            </div>
+                                        @endif
 
-                                    @if($service->name == 'Working Days')
-                                        <div class="col-xs-10">
-                                            <h2>
-                                                 <i class="fa fa-calendar"></i>
-                                            {{ $service->name }}
-                                            </h2>
-                                        <div>
+                                        @if($service->name == 'CataLex Sign')
+                                            <div class="col-xs-10">
+                                                <h2><i class="fa fa-search"></i> CataLex Sign Unlimited</h2>
 
-                                        <p>Calculates legal deadlines based on common definitions of “working day”.</p>
-                                        <p>This service is <strong>FREE</strong>.</p>
-                                    @endif
+                                                <div>
+                                                    <p>Digitally sign documents online.</p>
+                                                    <p>CataLex Sign is free for up to 10 signs per month. For unlimited signing, subscribe to CataLex Sign Unlimited for only <strong>$5 per month</strong>!</p>
+                                                </div>
+                                            </div>
+                                        @endif
 
-                                    @if($service->name == 'ConCat')
-                                        <div class="col-xs-10">
-                                            <h2>
-                                                 <i class="fa fa-copy"></i>
-                                            {{ $service->name }}
-                                            </h2>
-                                        <div>
+                                        @if($service->name == 'Working Days')
+                                            <div class="col-xs-10">
+                                                <h2><i class="fa fa-calendar"></i> Working Days</h2>
 
-                                        <p>Concatenates (combines) PDF documents quickly and securely.</p>
-                                        <p>This service is <strong>FREE</strong>.</p>
-                                    @endif
+                                                <div>
+                                                    <p>Calculates legal deadlines based on common definitions of “working day”.</p>
+                                                    <p>This service is <strong>FREE</strong>.</p>
+                                                </div>
+                                            </div>
+                                        @endif
 
-                                    </div>
-                                    </div>
+                                        @if($service->name == 'ConCat')
+                                            <div class="col-xs-10">
+                                                <h2><i class="fa fa-copy"></i> ConCat</h2>
+
+                                                <div>
+                                                    <p>Concatenates (combines) PDF documents quickly and securely.</p>
+                                                    <p>This service is <strong>FREE</strong>.</p>
+                                                </div>
+                                            </div>
+                                        @endif
 
                                     </div>
                                 </div>
