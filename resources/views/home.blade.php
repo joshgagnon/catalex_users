068e1489759370a3d459c41db4567628fcc3d9b0
Paddy Moran <pdmoran@outlook.com>
2017-08-16 10:48:04 +1200

    Add sign link to services page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,6 @@
 @endif
 
 <div class="services container">
-
     <div class="row">
         <div class="col-md-6">
             <div class="service">
@@ -29,6 +28,7 @@
                     <i class="fa fa-search"></i>
 
                     <h4>Law Browser</h4>
+
                     <p>Find law faster and easier than ever before. Definition recognition, automatic cross-references, side-by-side browsing, and more!</p>
                  </a>
             </div>
@@ -38,42 +38,54 @@
             <div class="service">
                 <a href="{{ route('good-companies-login') }}">
                     <i class="fa fa-briefcase"></i>
+
                     <h4>Good Companies</h4>
+
                     <p>Takes care of a company’s disclosure and administrative requirements under the Companies Act 1993.</p>
                 </a>
             </div>
         </div>
-
     </div>
-
 
     <div class="row">
         <div class="col-md-6">
             <div class="service">
-            <a href="http://workingdays.catalex.nz/">
-                 <i class="fa fa-calendar"></i>
-                <h4>Working Days</h4>
-                <p>
-                    Calculates legal deadlines based on common definitions of “working day”. 
-                </p>
-                 </a>
+                <a href="{{ route('sign-login')  }}">
+                    <i class="fa fa-pencil"></i>
+
+                    <h4>CataLex Sign</h4>
+
+                    <p>Digitally sign legal documents online.</p>
+                </a>
             </div>
         </div>
 
         <div class="col-md-6">
             <div class="service">
-            <a href="https://concat.catalex.nz/">
-                 <i class="fa fa-copy"></i>
-                <h4>ConCat</h4>
-                <p>
-                    Concatenates (combines) PDF documents quickly and securely.
-                </p>
+                <a href="http://workingdays.catalex.nz/">
+                    <i class="fa fa-calendar"></i>
+
+                    <h4>Working Days</h4>
+
+                    <p>Calculates legal deadlines based on common definitions of “working day”.</p>
                 </a>
             </div>
         </div>
-
     </div>
 
+    <div class="row">
+        <div class="col-md-6 col-sm-push-3">
+            <div class="service">
+                <a href="https://concat.catalex.nz/">
+                    <i class="fa fa-copy"></i>
+
+                    <h4>ConCat</h4>
+
+                    <p>Concatenates (combines) PDF documents quickly and securely.</p>
+                </a>
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
