18307258547d3482fcba6ccf477cba4f0721b7eb
Paddy Moran <pdmoran@outlook.com>
2017-09-01 11:23:14 +1200

    New sign email: documents signed and ready

---
+++ resources/views/emails/sign/signing-complete.blade.php
@@ -0,0 +1,33 @@
+@extends('emails.ink-template')
+
+@section('content')
+    <table class="container main">
+        <tr>
+            <td>
+                <table class="row">
+                    <tr>
+                        <td class="wrapper last">
+                            <table class="twelve columns">
+                                <tr>
+                                    <td>
+                                        <h2 class="center">Documents Signed & Ready</h2>
+
+                                        <p>Hi {{ $name }},</p>
+                                        <p>The document set "{{ $setDescription }}" has now be signed by all recipients</p>
+
+                                        @include('emails.layouts.partials.button', ['text' => 'View Documents', 'url' => $link])
+
+                                        <p>Kind regards</p>
+                                        <p>The CataLex team</p>
+
+                                        <p><a href="mailto:mail@catalex.nz">mail@catalex.nz</a></p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+@endsection
