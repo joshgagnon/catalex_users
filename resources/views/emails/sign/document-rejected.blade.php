5dc4d9bab19b76e956ab5eba963d0a768918b2f1
Paddy Moran <pdmoran@outlook.com>
2017-09-08 09:09:00 +1200

    Added sign rejection email

---
+++ resources/views/emails/sign/document-rejected.blade.php
@@ -0,0 +1,31 @@
+@extends('emails.layouts.non-member')
+
+@section('content')
+    <table class="container main">
+        <tr>
+            <td>
+                <table class="row">
+                    <tr>
+                        <td class="wrapper last">
+                            <table class="twelve columns">
+                                <tr>
+                                    <td>
+                                        <p>Hi {{ $recipientName }}</p>
+
+                                        <p>{{ $rejectorName }} has declined to sign one or more documents. Click below to view.</p>
+
+                                        @include('emails.layouts.partials.button', ['text' => 'View Documents', 'url' => $link])
+
+                                        <p>Best regards</p>
+                                        <p>The CataLex team</p>
+                                        <p><a href="mailto:mail@catalex.nz">mail@catalex.nz</a></p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+@endsection
