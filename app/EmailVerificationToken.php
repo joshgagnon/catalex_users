254dc2fa840163db6a7f6e8e3e546861031386c0
Paddy Moran <pdmoran@outlook.com>
2017-09-11 15:42:18 +1200

    Added email verification token migration and model

---
+++ app/EmailVerificationToken.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmailVerificationToken extends Model
+{
+    public static function createToken(User $user)
+    {
+        // Delete any existing tokens
+        EmailVerificationToken::where('user_id', '=', $user->id)->delete();
+
+        // Create a new instance with a random token
+        return EmailVerificationToken::forceCreate([
+            'user_id' => $user->id,
+            'token' => hash_hmac('sha256', Str::random(100), Config::get('APP_KEY'))
+        ]);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
